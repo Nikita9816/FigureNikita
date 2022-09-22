3ba9819ccb7e65dfcb6d1bc48ab3f67455fa3a33
pupil <pupil@ivkhk.ee>
2022-09-22 15:05:09 +0300

    master

---
+++ src/figurenikita/FigureNikita.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package figurenikita;
+
+/**
+ *
+ * @author pupil
+ */
+public class FigureNikita {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("----------FIGURE----------");
+        System.out.println("");
+        for(int i=0; i<8; i++){
+            for(int j=0; j<8; j++){
+                System.out.print("* ");
+                    }
+            System.out.println();{
+           
+        }
+        }
+        String s = "*";
+        for(int k=0; k<10; k++){
+            
+            
+            System.out.println(s);
+            s = "*" + s;
+        }
+    }
+    
+}
